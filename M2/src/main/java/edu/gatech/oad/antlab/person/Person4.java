1a5262c4bd109336f71508c758eccbf43ead204c
Sahiti Baddula <sahiti@lawn-128-61-53-18.lawn.gatech.edu>
2019-01-25 18:06:12 -0500

    remove main sahiti

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -9,9 +9,6 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person4 {
-   public static void main(String [] args){
-      System.out.println(calc("1bl2ah3"));
-   }
   /** Holds the persons real name */
   private String name;
     /**
